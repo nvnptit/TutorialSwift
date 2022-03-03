b9900049b0658d32dc6a8037e0b60ddf56aa83a1
nhatnv77 <nhatnv77@gmail.com>
2022-03-03 09:33:14 +0700

    Initial Commit

---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Nhat on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
